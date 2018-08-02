511393a01161495d096852c03902b5276eea1038
nehruperumalla1 <nehruperumalla@msitprogram.net>
2018-08-02 17:25:29 +0530

    " -> 0 of 5 passed. pylint: 1.18/10 "

---
+++ Module 4/p2/bob_counter.py
@@ -0,0 +1,24 @@
+'''Assume s is a string of lower case characters.
+
+Write a program that prints the number of times the string 'bob'
+occurs in s. For example, if s = 'azcbobobegghakl', then your program should print
+
+Number of times bob occurs is: 2'''
+
+def main():
+    s = input()
+    s1 = input()
+    l=len(s)
+    l1=len(s1)
+    i,j,c = 0,0,0
+    while i<l-1:
+        if s[i] == s1[j]:
+            j += 1
+            if j == l1-1:
+                c += 1
+                j = 0
+                i -= 1
+        i += 1
+    print(c)
+if __name__== "__main__":
+    main()
