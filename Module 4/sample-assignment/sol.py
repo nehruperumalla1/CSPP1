74023ee0ceae186f1179bfa961ddb59fe2662fbd
nehruperumalla1 <nehruperumalla@msitprogram.net>
2018-08-02 12:50:34 +0530

    " -> 1 of 1 passed. pylint: 10.00/10 "

--- Module 4/sample-assignment/sol.py
+++ Module 4/sample-assignment/sol.py
@@ -1,10 +1,11 @@
-# Exercise: Assignment-1
+''' Exercise: Assignment-1'''
 # Write a Python function, factorial(n), that takes in one number and
 # returns the factorial of given number.
 
 # This function takes in one number and returns one number.
 
 def main():
+    '''Program to sum two number'''
     data = input().split(",")
     print(int(data[0])+int(data[1]))
 
