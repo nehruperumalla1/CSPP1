3ed87e69c4ef0ea5005e07da8a7cd31a41d87937
nehruperumalla1 <nehruperumalla@msitprogram.net>
2018-08-02 17:43:20 +0530

    " -> 5 of 5 passed. pylint: 10.00/10 "

--- Module 4/p2/bob_counter.py
+++ Module 4/p2/bob_counter.py
@@ -11,15 +11,15 @@ def main():
     sub_str = "bob"
     length_main = len(main_str)
     length_sub = len(sub_str)
-    i, j, c = 0, 0, 0
+    i, j, count = 0, 0, 0
     while i < length_main-1:
         if main_str[i] == sub_str[j]:
             j += 1
             if j == length_sub-1:
-                c += 1
+                count += 1
                 j = 0
                 i -= 1
         i += 1
-    print(c)
+    print(count)
 if __name__ == "__main__":
     main()
