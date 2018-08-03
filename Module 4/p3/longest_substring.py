d3badc79d2e2bb1ab215bee337aa610bb67411cc
nehruperumalla1 <nehruperumalla@msitprogram.net>
2018-08-03 09:12:12 +0530

    " -> 5 of 5 passed. pylint: 8.12/10 "

--- Module 4/p3/longest_substring.py
+++ Module 4/p3/longest_substring.py
@@ -1,6 +1,7 @@
 '''Assume s is a string of lower case characters.
 
-Write a program that prints the longest substring of s in which the letters occur in alphabetical order.
+Write a program that prints the longest substring of s in which the letters occur 
+in alphabetical order.
 For example, if s = 'azcbobobegghakl', then your program should print
 
 Longest substring in alphabetical order is: beggh
@@ -11,7 +12,8 @@ For example, if s = 'abcbcd', then your program should print
 Longest substring in alphabetical order is: abc
 
 Note: This problem may be challenging. We encourage you to work smart.
-If you've spent more than a few hours on this problem, we suggest that you move on to a different part of the course.
+If you've spent more than a few hours on this problem, we suggest that you move on to a 
+different part of the course.
 If you have time, come back to this problem after you've had a break and cleared your head.'''
 
 def main():
@@ -31,5 +33,5 @@ def main():
     string = j-temp2
     print(main[string:j+1])
 
-if __name__== "__main__":
+if __name__ == "__main__":
     main()
