b8b594d44c782e6ca8108ea0e5f38eb1ecd02ae9
nehruperumalla1 <nehruperumalla@msitprogram.net>
2018-08-03 15:59:46 +0530

    " -> 3 of 3 passed. pylint: 7.14/10 "

--- Module 5/p3/square_root_bisection.py
+++ Module 5/p3/square_root_bisection.py
@@ -1,4 +1,5 @@
-# Write a python program to find the square root of the given number
+'''Write a python program to find the square root of the
+given number'''
 # using approximation method
 
 # testcase 1
@@ -10,18 +11,19 @@
 # output: 6.999999999999991
 
 def main():
-	sroot = int(input())
-	epsilon = 0.01
-	#step = 0.1
-	start=0
-	end=sroot
-	root=(start+end)/2
-	while abs(root**2-sroot) >= epsilon:
-		if root**2 < sroot:
-			start = root
-		else:
-			end = root
-		root = (start+end)/2
-	print(str(root))
+    '''Square root using bisection'''
+    sroot = int(input())
+    epsilon = 0.01
+    #step = 0.1
+    start=0
+    end=sroot
+    root=(start+end)/2
+    while abs(root**2-sroot) >= epsilon:
+        if root**2 < sroot:
+            start = root
+        else:
+            end = root
+        root = (start+end)/2
+    print(str(root))
 if __name__== "__main__":
-	main()
+    main()
