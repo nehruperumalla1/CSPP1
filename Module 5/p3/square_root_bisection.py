5c4a3a89c0159d46f97a8814ca9611a6ea1f36f4
nehruperumalla1 <nehruperumalla@msitprogram.net>
2018-08-03 16:00:10 +0530

    " -> 3 of 3 passed. pylint: 10.00/10 "

--- Module 5/p3/square_root_bisection.py
+++ Module 5/p3/square_root_bisection.py
@@ -15,9 +15,9 @@ def main():
     sroot = int(input())
     epsilon = 0.01
     #step = 0.1
-    start=0
-    end=sroot
-    root=(start+end)/2
+    start = 0
+    end = sroot
+    root = (start+end)/2
     while abs(root**2-sroot) >= epsilon:
         if root**2 < sroot:
             start = root
@@ -25,5 +25,5 @@ def main():
             end = root
         root = (start+end)/2
     print(str(root))
-if __name__== "__main__":
+if __name__ == "__main__":
     main()
