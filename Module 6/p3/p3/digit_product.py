18d72af88eb609d5658f84253c2891c88b0d3104
nehruperumalla1 <nehruperumalla@msitprogram.net>
2018-08-04 11:34:26 +0530

    " -> 7 of 7 passed. pylint: 10.00/10 "

--- Module 6/p3/p3/digit_product.py
+++ Module 6/p3/p3/digit_product.py
@@ -1,18 +1,18 @@
 '''
 Given a  number int_input, find the product of all the digits
-example: 
+example:
     input: 123
     output: 6
 '''
 def main():
     '''Input a number it will return product of all the digits in that number'''
     int_input = int(input())
-    y_1=abs(int_input)
+    y_1 = abs(int_input)
     x_1 = int_input
     mul = 1
     if y_1 == 0:
         print("0")
-    while y_1!=0:
+    while y_1 != 0:
         rem = y_1%10
         mul = mul*rem
         y_1 = y_1//10
