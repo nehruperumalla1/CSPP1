8f722b7e11685649dcf64ff0560ca360077982e9
nehruperumalla1 <nehruperumalla@msitprogram.net>
2018-08-04 11:08:35 +0530

    " -> 7 of 7 passed. pylint: 10.00/10 "

--- Module 6/p1/p1/fizz_buzz.py
+++ Module 6/p1/p1/fizz_buzz.py
@@ -2,7 +2,7 @@
 Write a short program that prints each number from 1 to num on a new line.
 For each multiple of 3, print "Fizz" instead of the number.
 For each multiple of 5, print "Buzz" instead of the number.
-For numbers which are multiples of both 3 and 5, print "FizzBuzz" instead of the 
+For numbers which are multiples of both 3 and 5, print "FizzBuzz" instead of the
 number.
 '''
 def main():
