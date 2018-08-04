2a3b109e52000828751ac42bfeda72c2ee77180a
nehruperumalla1 <nehruperumalla@msitprogram.net>
2018-08-04 11:33:59 +0530

    " -> 7 of 7 passed. pylint: 8.24/10 "

--- Module 6/p3/p3/digit_product.py
+++ Module 6/p3/p3/digit_product.py
@@ -7,18 +7,18 @@ example:
 def main():
     '''Input a number it will return product of all the digits in that number'''
     int_input = int(input())
-    y=abs(int_input)
-    x = int_input
+    y_1=abs(int_input)
+    x_1 = int_input
     mul = 1
-    if y == 0:
-    	print("0")
-    while y!=0:
-        rem = y%10
+    if y_1 == 0:
+        print("0")
+    while y_1!=0:
+        rem = y_1%10
         mul = mul*rem
-        y = y//10
-    if x > 0:
-    	print(mul)
+        y_1 = y_1//10
+    if x_1 > 0:
+        print(mul)
     elif int_input < 0:
-    	print("-"+str(mul))
+        print("-"+str(mul))
 if __name__ == "__main__":
     main()
