ceb755380224202208418718578c29c5a4ace470
nehruperumalla1 <nehruperumalla@msitprogram.net>
2018-08-07 00:41:55 +0530

    " -> 0 of 10 passed. pylint: 0/0 "

--- Module 7/Functions - Assignment-2/assignment2.py
+++ Module 7/Functions - Assignment-2/assignment2.py
@@ -1,11 +1,10 @@
-# Assignment-2 - Paying Debt off in a Year
+'''Assignment-2 - Paying Debt off in a Year'''
 
 # Now write a program that calculates the minimum fixed monthly payment needed in order pay off
 #a credit card balance within 12 months.
-# By a fixed monthly payment, we mean a single number which does not change each month, but 
+# By a fixed monthly payment, we mean a single number which does not change each month, but
 #instead is a constant amount that will be
 # paid each month.
-
 # In this problem, we will not be dealing with a minimum monthly payment rate.
 
 # The following variables contain values as described below:
@@ -30,6 +29,7 @@
 
 
 def payingdebtoffinayear(balance, annualinterestrate):
+	'''Lowest Payment'''
     i_bal = balance
     minfixedmonthlypayment = 10
     monthlyinterestrate = annualinterestrate/12
@@ -46,6 +46,7 @@ def payingdebtoffinayear(balance, annualinterestrate):
     return minfixedmonthlypayment-10
 
 def main():
+	'''Lowest Payment main function'''
     data = input()
     data = data.split(' ')
     data = list(map(float, data))
