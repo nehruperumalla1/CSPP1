c754cd049c000ccf45a185fdbde1726801fe74a3
nehruperumalla1 <nehruperumalla@msitprogram.net>
2018-08-07 00:45:52 +0530

    " -> 10 of 10 passed. pylint: 10.00/10 "

--- Module 7/Functions - Assignment-2/assignment2.py
+++ Module 7/Functions - Assignment-2/assignment2.py
@@ -50,6 +50,6 @@ def main():
     data = input()
     data = data.split(' ')
     data = list(map(float, data))
-    print("Lowest Payment: "+str(payingdebtoffinayear(data[0], data[1]))) 
+    print("Lowest Payment: "+str(payingdebtoffinayear(data[0], data[1])))
 if __name__ == "__main__":
     main()
