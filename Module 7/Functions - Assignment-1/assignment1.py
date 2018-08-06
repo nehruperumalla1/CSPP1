35d53eb2f4d2f588f01efec2437261cc55fc2806
nehruperumalla1 <nehruperumalla@msitprogram.net>
2018-08-06 21:34:59 +0530

    " -> 10 of 10 passed. pylint: -1.43/10 "

--- Module 7/Functions - Assignment-1/assignment1.py
+++ Module 7/Functions - Assignment-1/assignment1.py
@@ -1,6 +1,7 @@
 # Functions | Assignment-1 - Paying Debt off in a Year
 
-# Write a program to calculate the credit card balance after one year if a person only pays the minimum monthly payment required by the
+# Write a program to calculate the credit card balance after one year if a person only
+#pays the minimum monthly payment required by the
 # credit card company each month.
 
 # The following variables contain values as described below:
@@ -8,38 +9,41 @@
 # annualInterestRate - annual interest rate as a decimal
 # monthlyPaymentRate - minimum monthly payment rate as a decimal
 
-# For each month, calculate statements on the monthly payment and remaining balance. At the end of 12 months, print out the remaining
+# For each month, calculate statements on the monthly payment and remaining balance.
+#At the end of 12 months, print out the remaining
 # balance. Be sure to print out no more than two decimal digits of accuracy - so print
 
 # Remaining balance: 813.41
 # instead of
 # Remaining balance: 813.4141998135
 
-# So your program only prints out one thing: the remaining balance at the end of the year in the format:
+# So your program only prints out one thing: the remaining balance at the end of
+#the year in the format:
 # Remaining balance: 4784.0
 
 # A summary of the required math is found below:
 # Monthly interest rate= (Annual interest rate) / 12.0
 # Minimum monthly payment = (Minimum monthly payment rate) x (Previous balance)
 # Monthly unpaid balance = (Previous balance) - (Minimum monthly payment)
-# Updated balance each month = (Monthly unpaid balance) + (Monthly interest rate x Monthly unpaid balance)
+# Updated balance each month = (Monthly unpaid balance) + (Monthly interest rate x
+#Monthly unpaid balance)
 
 
-def payingDebtOffInAYear(balance, annualInterestRate, monthlyPaymentRate):
-	for i in range(12):
-		monthlyInterestRate = annualInterestRate/12
-		minMonthlyPayment = monthlyPaymentRate*balance
-		monthlyUnpaidBalance = balance-minMonthlyPayment
-		balance = monthlyUnpaidBalance+(monthlyInterestRate*monthlyUnpaidBalance)
-	print("Remaining balance: "+str(round(balance,2)))
-	
+def payingDebtOffInAYear(balance, annualInterest_Rate, monthlyPayment_Rate):
+    for i in range(12):
+        monthlyInterest_Rate = annualInterest_Rate/12
+        minMonthly_Payment = monthlyPayment_Rate*balance
+        monthlyUnpaid_Balance = balance-minMonthly_Payment
+        balance = monthlyUnpaid_Balance+(monthlyInterest_Rate*monthlyUnpaid_Balance)
+    print("Remaining balance: "+str(round(balance,2)))
+    
 
 def main():
-	data = input()
-	data = data.split(' ')
-	data = list(map(float, data))
-	payingDebtOffInAYear(data[0],data[1],data[2])
+    data = input()
+    data = data.split(' ')
+    data = list(map(float, data))
+    payingDebtOffInAYear(data[0],data[1],data[2])
 
 if __name__== "__main__":
-	main()
+    main()
 
