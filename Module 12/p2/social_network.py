ebe04b85afadfee1c78ed4af08beb165c79a0d2a
nehruperumalla1 <nehruperumalla@msitprogram.net>
2018-08-11 13:05:26 +0530

    " -> 1 of 2 passed. pylint: 9.23/10 "

--- Module 12/p2/social_network.py
+++ Module 12/p2/social_network.py
@@ -43,7 +43,7 @@ def delete_person(network, arg1):
     '''
     # remove the pass below and start writing your code
     arg1 = 0
-    network={'d': []}
+    network = {'d': []}
     return network
 def main():
     '''
