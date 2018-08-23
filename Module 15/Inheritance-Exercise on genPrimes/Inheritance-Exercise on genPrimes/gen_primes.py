8723d94aaf2b24a6aad35d637c4a9921e6e73c34
nehruperumalla1 <nehruperumalla@msitprogram.net>
2018-08-23 11:38:11 +0530

    " -> 1 of 8 passed. pylint: 7.67/10 "

---
+++ Module 15/Inheritance-Exercise on genPrimes/Inheritance-Exercise on genPrimes/gen_primes.py
@@ -0,0 +1,32 @@
+#define the gen_primes function here
+# def prime(n):
+
+# 	for a in range(2,n):
+# 		if n%a == 0:
+# 			return False
+# 	return True
+
+def genPrimes():
+	num = 2
+	while num>0:
+		prime = True
+		for index in range(2,num):
+			if num%index == 0:
+				prime = False
+		if prime:
+			yield num
+		num += 1
+
+def main():
+	data = input()
+	l=data.split()
+	a=int(l[0])
+	b=int(l[1])
+	primeGenerator = genPrimes()
+	for i in range(a):
+	    p=primeGenerator.__next__()
+	    if(i%b==0):
+	        print(p)
+	
+if __name__== "__main__":
+	main()
---
+++ Module 20/CodeCampMatrixOperations/matrix_operations.py
@@ -0,0 +1,78 @@
+import copy
+def mult_matrix(m1, m2):
+    '''
+        check if the matrix1 columns = matrix2 rows
+        mult the matrices and return the result matrix
+        print an error message if the matrix shapes are not valid for mult
+        and return None
+        error message should be "Error: Matrix shapes invalid for mult"
+    '''
+    matmul = copy.deepcopy(m1)
+    for index in range(len(m1)):
+        for jindex in range(len(m2)):
+            matmul[index][jindex] = m1[index][jindex]*m2[index][jindex]
+    return matmul
+
+def add_matrix(m1, m2):
+    '''
+        check if the matrix shapes are similar
+        add the matrices and return the result matrix
+        print an error message if the matrix shapes are not valid for addition
+        and return None
+        error message should be "Error: Matrix shapes invalid for addition"
+    '''
+    matsum = copy.deepcopy(m1)
+    for index in range(len(m1)):
+        for jindex in range(len(m2)):
+            matsum[index][jindex] += m2[index][jindex]
+
+    return matsum
+
+def read_matrix(matlen):
+    '''
+        read the matrix dimensions from input
+        create a list of lists and read the numbers into it
+        in case there are not enough numbers given in the input
+        print an error message and return None
+        error message should be "Error: Invalid input for the matrix"
+    '''
+    roww = []
+    for index in range(matlen[1]):
+        row = input()
+        row = list(map(int, row.split(' ')))
+        roww.append(row)
+    if matlen[0] != len(roww):
+        return None
+    for index in roww:
+        if len(index) != matlen[1]:
+            return None
+    return roww
+# def input_format(matrix, matrixlst):
+
+def main():
+    # read matrix 1
+    matlen1 = input()
+    matlen1 = list(map(int, matlen1.split(',')))
+    matrix1 = read_matrix(matlen1)
+    matlen2 = input()
+    matlen2 = list(map(int, matlen2.split(',')))
+    matrix2 = read_matrix(matlen2)
+    if matrix1 == None or matrix2 == None:
+        print("Error: Invalid input for the matrix")
+    else:
+        print(matrix1)
+        print(matrix2)
+        matrix_add = add_matrix(matrix1, matrix2)
+        print(matrix_add)
+        matrix_mult = mult_matrix(matrix1, matrix2)
+        print(matrix_mult)
+
+    # read matrix 2
+
+    # add matrix 1 and matrix 2
+
+    # multiply matrix 1 and matrix 2
+    pass
+
+if __name__ == '__main__':
+    main()
