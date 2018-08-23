475b574a003b533a1bd1d723f0c66e0a1e2082d3
nehruperumalla1 <nehruperumalla@msitprogram.net>
2018-08-23 14:57:18 +0530

    " -> 7 of 8 passed. pylint: 9.63/10 "

--- Module 20/CodeCampMatrixOperations/matrix_operations.py
+++ Module 20/CodeCampMatrixOperations/matrix_operations.py
@@ -56,10 +56,10 @@ def read_matrix(matlen):
         row = list(map(int, row.split(' ')))
         roww.append(row)
     if matlen[0] != len(roww):
-        return True
+        return None
     for index in roww:
         if len(index) != matlen[1]:
-            return True
+            return None
     return roww
 # def input_format(matrix, matrixlst):
 
@@ -72,7 +72,7 @@ def main():
     matlen2 = input()
     matlen2 = list(map(int, matlen2.split(',')))
     matrix2 = read_matrix(matlen2)
-    if matrix1 and matrix2:
+    if matrix1 == None or matrix2 == None:
         print("Error: Invalid input for the matrix")
     else:
         matrix_add = add_matrix(matrix1, matrix2)
