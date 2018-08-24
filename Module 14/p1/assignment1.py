f1947133cac37a93e49d30ac447432031f9256b1
nehruperumalla1 <nehruperumalla@msitprogram.net>
2018-08-24 17:27:47 +0530

    " -> 0 of 5 passed. pylint: 0.24/10 "

--- Module 14/p1/assignment1.py
+++ Module 14/p1/assignment1.py
@@ -61,10 +61,10 @@ def load_words(file_name):
     in_file.close()
     return word_list
 
-WORDLIST_FILENAME = 'words.txt
+WORDLIST_FILENAME = 'words.txt'
 class Message:
 	def __init__(self, text):
-		self.message_text = text
+		self.message_text = text1
 		self.valid_words = load_words("words.txt")
 	def get_message_text(self):
 		return self.message_text
