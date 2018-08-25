a091ac7255257827e8ccf877a24a41351fde8db6
nehruperumalla1 <nehruperumalla@msitprogram.net>
2018-08-25 11:47:08 +0530

    " -> 0 of 3 passed. pylint: 4.44/10 "

--- Module 22/assignment2/assignment2/clean_input.py
+++ Module 22/assignment2/assignment2/clean_input.py
@@ -4,7 +4,7 @@ alphabets in both upper and lower case and numbers.
 '''
 import re
 def clean_string(string):
-	string1 = re.sub('[^a-z ]', '', string)
+	string1 = re.sub('[^A-z ]', '', string)
 	return string1.strip()
 
 def main():
