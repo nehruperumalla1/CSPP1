ec93dfc003007d32aab7ffbbc11060890aafef3b
nehruperumalla1 <nehruperumalla@msitprogram.net>
2018-10-10 17:05:44 +0530

    " -> 26 of 26 passed. pylint: 9.68/10 "

--- Module 22/Check Sudoku/Check Sudoku/check_sudoku1.py
+++ Module 22/Check Sudoku/Check Sudoku/check_sudoku1.py
@@ -1,37 +1,3 @@
-'''
-    Sudoku is a logic-based, combinatorial number-placement puzzle.
-    The objective is to fill a 9×9 grid with digits so that
-    each column, each row, and each of the nine 3×3 subgrids that compose the grid
-    contains all of the digits from 1 to 9.
-
-    Complete the check_sudoku function to check if the given grid
-    satisfies all the sudoku rules given in the statement above.
-'''
-
-# def check_sudoku(sudoku):
-#     '''
-#         Your solution goes here. You may add other helper functions as needed.
-#         The function has to return True for a valid sudoku grid and false otherwise
-#     '''
-#     count = 0
-#     count1 = 0
-#     for index in sudoku:
-#         if len(set(index)) == 9:
-#             count1 += 1
-#     set1 = set()
-#     for iindex in range(len(sudoku)):
-#         for jindex in range(len(sudoku[iindex])):
-#             for val in range(9):
-#                 if jindex == val:
-#                     # print(val)
-#                     set1.add(sudoku[iindex][jindex])
-#                     if len(set1) == 9:
-#                         # print(len(set1))
-#                         count += 1
-#     # print(count)
-#     if count1 == 9 and count == 9:
-#         return True
-#     return False
 def check_col(sudoku):
     '''
     Function to check columns
@@ -67,7 +33,6 @@ def check_sudoku(sudoku):
     # if check_grid(sudoku) is False:
     #     return False    
     return True
-
 def main():
     '''
         main function to read input sudoku from console
@@ -77,11 +42,11 @@ def main():
     sudoku = []
 
     # loop to read 9 lines of input from console
-    for _ in range(9):
+    for i_num in range(9):
         # read a line, split it on SPACE and append row to list
         row = input().split(' ')
-        #print(row)
         sudoku.append(row)
+        i_num += 1
     # call solution function and print result to console
     print(check_sudoku(sudoku))
 
